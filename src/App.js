1ad8be140e8263ca466c8e92ae9e2326c6d55cfd
julietadicio <julietadicio@gmail.com>
2022-10-18 15:47:32 -0300

    empezando layour e-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <nav>
+        <ul>
+          <li href="#">
+            HOME
+          </li>
+          <li>
+            TIERRA
+          </li>
+          <li>
+            SEMILLAS
+          </li>
+          <li>
+            FERTILIZANTES
+          </li>
+        </ul>
+      </nav>
+
+      <div id="section1">
+        <h1>FACTOR TIERRA</h1>
+        <a class="btn" href="#section3">Ir seccion3</a>
+      </div>
+
+      <div id="section2">
+        <h2>seccion 2</h2>
+      </div>
+
+      <div id="section3">
+        <h2>seccion 3</h2>
+        <a href="#section1">Ir arriba</a>
+      </div>
+
+    </>
   );
 }
 
 export default App;
+
