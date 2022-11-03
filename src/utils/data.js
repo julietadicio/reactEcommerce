3606eee83cb90c43372f6c15d9eea9db0d722165
julietadicio <julietadicio@gmail.com>
2022-11-03 18:13:13 -0300

    Emepzando creación de prodcutos en data.js

---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        id: 1,
+        name: "Tierra Preparada",
+        description: "La tierra preparada es la recomendada para el momento de sembrar una nueva planta en una maceta, de esta forma puedes contar con la seguridad de que su proceso de crecimiento será el óptimo desde sus inicios.",
+        stock: 20,
+        cost: 500,
+        image: 'https://promart.vteximg.com.br/arquivos/ids/223539-1000-1000/105936.jpg?v=636178357627370000',
+        category: 'Tierra',
+    },
+    
+    {
+        id: 2,
+        name: "Tierra Profesional",
+        description: 'Este tipo de tierra mejora la retención de humedad y activa los sistemas naturales de protección de las plantas. Tiene importantes porcentajes de nitrógeno, elemento ideal para cultivar plantas. La tierra profesional cuenta también con una mezcla de turba negra, musgo de turba, granulado y perlita. Por eso se puede utilizar en cualquier cultivo, tanto en espacios interiores como exteriores.',
+        stock: 15,
+        cost: 800,
+        image: 'https://http2.mlstatic.com/D_NQ_NP_613315-MLA43775148435_102020-W.jpg',
+        category: 'Tierra',
+    },
+    {
+        id: 3,
+        name: "Tierra Orgánica",
+        description: 'La tierra orgánica mejora la retención humedad y da mayor resistencia a la planta. Una gran ventaja es que se puede usar en todo tipo de plantas. La tierra orgánica forma parte del abono que se le debe dar a las plantas con el objetivo de que su crecimiento sea más sano.',
+        stock: 22,
+        cost: 720,
+        image: 'https://www.indacopolietileno.com.ar/wp-content/uploads/2020/09/117305177_631167117779895_8637520859369536239_n.jpg',
+        category: 'Tierra',
+    },
+    {
+        id: 4,
+        name: "Tierra de Hoja",
+        description: 'La tierra de hoja mejora la textura del suelo y permite un mejor paso de aire interno. La tierra de hoja es considerada como el “oro negro”de las plantas debido a que tiene la capacidad de aportar gran cantidad de nutrientes (micronutrientes y macronutrientes).',
+        stock: 18,
+        cost: 850,
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcornershopapp.com%2Fes-cl%2Fproducts%2F1j1ou-tierra-de-hojas-reforzada-bolsa-de-15-lt&psig=AOvVaw1-9e-LI47OBH3fHakrMKQh&ust=1667596238984000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKD2jJ32kvsCFQAAAAAdAAAAABAF',
+        category: 'Tierra',
+    },
+    
+]
