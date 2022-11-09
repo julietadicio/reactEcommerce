1014ad53b065e4618116decabb03348c63b2c6c0
julietadicio <julietadicio@gmail.com>
2022-11-09 12:09:20 -0300

    cambiando archivos a .jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <>
+        <h1 className="title">FACTOR TIERRA</h1>
+        <h1>Todo lo que necesitas para armar la huerta de tus sueños</h1>
+        
+      </>
+    );
+  };
+  
+  export default Home;
