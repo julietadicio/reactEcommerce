f29b94120aab20320bba4f77ac7fcbc58d2f8e7b
julietadicio <julietadicio@gmail.com>
2022-11-24 16:16:27 -0300

    funcionalidad de agregar una sola vez los productos iguales

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,18 +3,30 @@ import { useContext } from "react";
 import { CartContext } from "./CartContext";
 
 const Cart = () => {
-    const {cartList} = useContext(CartContext);
+    const { cartList } = useContext(CartContext);
+    const test = useContext(CartContext)
     console.log(cartList)
 
-    return(
+    return (
         <>
-        <h1>CART</h1>
-        {
-            cartList.length === 0
-            ? <h2>Tu carrito esta vacío</h2>
-            : cartList.map(item => <li>{item.name}</li>)
-        }
-        <Link to='/'><button>CONTINUE SHOPPING</button></Link>
+            <h1>CART</h1>
+            {
+                cartList.length === 0
+                    ? <h2>Tu carrito esta vacío</h2>
+                    : cartList.map(item => <div>
+                        <img src={item.itemImg} alt="" />
+                        <h1>{item.itemName} </h1>
+                        <h2>{item.itemCost}</h2>
+                        <h2>{item.itemQty}</h2>
+                        </div> )
+            }
+            <Link to='/'><button>CONTINUE SHOPPING</button></Link>
+            {
+                test.cartList.length > 0
+                ? <button onClick={test.clear}>Delete all</button>
+                : <h2></h2>
+            }
+            
         </>
     )
 }
--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -5,15 +5,35 @@ export const CartContext = createContext()
 const CartContextProvider = (props) => {
     const [cartList, setCartList] = useState([])
 
-    const addToCart = (item) => {
-        setCartList([
-            ...cartList,
-            item
-        ])
+    const addToCart = (item, qty) => {
+        let found = cartList.find(product => product.itemId === item.id);
+        if (found === undefined) {
+            setCartList([
+                ...cartList,
+                {
+                    itemId: item.id,
+                    itemImg: item.image[0],
+                    itemName: item.name,
+                    itemCost: item.cost,
+                    itemQty: qty
+                }
+            ])
+        } else {
+            found.qtyItem += qty;
+        }
+    }
+
+    const clear = () => {
+        setCartList([])
+    }
+
+    const deleteItem = (id) => {
+        let result = cartList.filter(item => item.idItem != id);
+        setCartList(result);
     }
 
     return (
-        <CartContext.Provider value={{cartList, addToCart}}>
+        <CartContext.Provider value={{ cartList, addToCart, clear, deleteItem }}>
             {props.children}
         </CartContext.Provider>
     )
